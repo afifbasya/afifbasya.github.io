c02a3e931a1ab3f6594e959eb13132b260c4eaaa
afifuddin <afifbasya@gmail.com>
2020-03-22 21:42:17 +0700

    Updates

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const setFilter = filter => ({
-  type: "SET_FILTER",
-  filter
-});
-
-export const setHover = (id, active) => ({
-  type: "SET_HOVER",
-  payload: { id, active }
-});
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-import filter from './filter'
-import items from './items'
-
-export default combineReducers({
-  filter, items
-})
