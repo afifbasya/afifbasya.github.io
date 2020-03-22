425e52c5fabccb759a67fc1e56568b241d2881db
afifuddin <afifbasya@gmail.com>
2020-03-22 21:33:51 +0700

    Updates

---
+++ precache-manifest.fd4359e17eac677f8560adbf1effbcad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc72daf739be5c3e54445776b88a5101",
+    "url": "/afifbasya.github.io/index.html"
+  },
+  {
+    "revision": "bf4eaea45be88395f051",
+    "url": "/afifbasya.github.io/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b607d345c430c48e56c5",
+    "url": "/afifbasya.github.io/static/css/main.e67ccbb9.chunk.css"
+  },
+  {
+    "revision": "bf4eaea45be88395f051",
+    "url": "/afifbasya.github.io/static/js/2.e5b9e815.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/afifbasya.github.io/static/js/2.e5b9e815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b607d345c430c48e56c5",
+    "url": "/afifbasya.github.io/static/js/main.fa442013.chunk.js"
+  },
+  {
+    "revision": "0a5536dec6fa7220efb4",
+    "url": "/afifbasya.github.io/static/js/runtime-main.c78ccca0.js"
+  },
+  {
+    "revision": "c6b59ae86ce330728b6e2f881bb7de75",
+    "url": "/afifbasya.github.io/static/media/afif.c6b59ae8.webp"
+  },
+  {
+    "revision": "9f26cf26d03e310dcfd07137486ddd06",
+    "url": "/afifbasya.github.io/static/media/cahaya.9f26cf26.webp"
+  },
+  {
+    "revision": "b1e42209ade896ec00d6e5f73da185bb",
+    "url": "/afifbasya.github.io/static/media/cipta.b1e42209.webp"
+  },
+  {
+    "revision": "59d4e3775b82c8e1690713b58ce83f83",
+    "url": "/afifbasya.github.io/static/media/ojs.59d4e377.webp"
+  },
+  {
+    "revision": "d42d9534aaf815b891eae37ddc94cd32",
+    "url": "/afifbasya.github.io/static/media/pmb.d42d9534.webp"
+  },
+  {
+    "revision": "8145ed6836e91550c400def7d490b5bf",
+    "url": "/afifbasya.github.io/static/media/simakad.8145ed68.webp"
+  },
+  {
+    "revision": "4147c8bd06cd41f6a98c3713cd8186fe",
+    "url": "/afifbasya.github.io/static/media/simkeu.4147c8bd.webp"
+  },
+  {
+    "revision": "392519dd710ed9c09e48045e177aa8c5",
+    "url": "/afifbasya.github.io/static/media/tracer.392519dd.webp"
+  },
+  {
+    "revision": "b5e9a39a9147145dc430482191619de2",
+    "url": "/afifbasya.github.io/static/media/waykanan.b5e9a39a.webp"
+  }
+]);
