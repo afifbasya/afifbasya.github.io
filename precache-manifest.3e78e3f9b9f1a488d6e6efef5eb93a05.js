2c94dcd0fc60cd3fab01e5009c1a6bbca0909b18
afifuddin <afifbasya@gmail.com>
2020-12-22 11:52:48 +0700

    Updates

--- precache-manifest.3e78e3f9b9f1a488d6e6efef5eb93a05.js
+++ precache-manifest.3e78e3f9b9f1a488d6e6efef5eb93a05.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc72daf739be5c3e54445776b88a5101",
+    "revision": "8377a4d22b85a2a86056f2b4396b3239",
     "url": "/afifbasya.github.io/index.html"
   },
   {
-    "revision": "bf4eaea45be88395f051",
+    "revision": "312adba7d46a3e033f51",
     "url": "/afifbasya.github.io/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "b607d345c430c48e56c5",
+    "revision": "4912ca562d4e08516a07",
     "url": "/afifbasya.github.io/static/css/main.e67ccbb9.chunk.css"
   },
   {
-    "revision": "bf4eaea45be88395f051",
-    "url": "/afifbasya.github.io/static/js/2.e5b9e815.chunk.js"
+    "revision": "312adba7d46a3e033f51",
+    "url": "/afifbasya.github.io/static/js/2.f0ddb122.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/afifbasya.github.io/static/js/2.e5b9e815.chunk.js.LICENSE.txt"
+    "url": "/afifbasya.github.io/static/js/2.f0ddb122.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b607d345c430c48e56c5",
-    "url": "/afifbasya.github.io/static/js/main.fa442013.chunk.js"
+    "revision": "4912ca562d4e08516a07",
+    "url": "/afifbasya.github.io/static/js/main.56c52536.chunk.js"
   },
   {
     "revision": "0a5536dec6fa7220efb4",
@@ -32,6 +32,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/afifbasya.github.io/static/media/afif.c6b59ae8.webp"
   },
   {
+    "revision": "5b09d8b82f2af49202d7bfd41a0a2a7a",
+    "url": "/afifbasya.github.io/static/media/bahanbatik.5b09d8b8.webp"
+  },
+  {
     "revision": "9f26cf26d03e310dcfd07137486ddd06",
     "url": "/afifbasya.github.io/static/media/cahaya.9f26cf26.webp"
   },
@@ -40,12 +44,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/afifbasya.github.io/static/media/cipta.b1e42209.webp"
   },
   {
+    "revision": "3d1f62690fa0ff7801b34532a73ad6c4",
+    "url": "/afifbasya.github.io/static/media/cmsbafmobile.3d1f6269.webp"
+  },
+  {
+    "revision": "26d76281b9acb0f0d8081e9f8facedca",
+    "url": "/afifbasya.github.io/static/media/incentive.26d76281.webp"
+  },
+  {
     "revision": "59d4e3775b82c8e1690713b58ce83f83",
     "url": "/afifbasya.github.io/static/media/ojs.59d4e377.webp"
   },
   {
     "revision": "d42d9534aaf815b891eae37ddc94cd32",
     "url": "/afifbasya.github.io/static/media/pmb.d42d9534.webp"
+  },
+  {
+    "revision": "8dd823e77fe2bd13d126b3e0d87be147",
+    "url": "/afifbasya.github.io/static/media/sandia-merchant.8dd823e7.webp"
   },
   {
     "revision": "8145ed6836e91550c400def7d490b5bf",
